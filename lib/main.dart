bd67a8107469e0dddf4c16d61afe373b1b7f0969
Davi Borges <davi.borges.developer@gmail.com>
2023-01-18 14:46:55 -0300

    Finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:projeto_perguntas/questao.dart';
+import 'package:projeto_perguntas/questionario.dart';
 import 'package:projeto_perguntas/resposta.dart';
+import 'package:projeto_perguntas/resultado.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,52 +28,72 @@ class PerguntaApp extends StatefulWidget {
 }
 
 class _PerguntaAppState extends State<PerguntaApp> {
-  var _perguntaselecionada = 0;
+  var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
 
   final List<Map<String, dynamic>> _perguntas = [
     {
       'texto': 'Qual é a sua cor favorita?',
-      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      'respostas': [
+        {'texto': 'Preto', 'ponto': 10},
+        {'texto': 'Vermelho', 'ponto': 5},
+        {'texto': 'Verde', 'ponto': 3},
+        {'texto': 'Branco', 'ponto': 1},
+      ],
     },
     {
       'texto': 'Qual é o seu animal favorito?',
-      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      'respostas': [
+        {'texto': 'Coelho', 'ponto': 10},
+        {'texto': 'Cobra', 'ponto': 5},
+        {'texto': 'Elefante', 'ponto': 3},
+        {'texto': 'Leão', 'ponto': 1},
+      ],
     },
     {
       'texto': 'Qual é o seu instrutor favorito?',
-      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+      'respostas': [
+        {'texto': 'Maria', 'ponto': 10},
+        {'texto': 'João', 'ponto': 5},
+        {'texto': 'Leo', 'ponto': 3},
+        {'texto': 'Pedro', 'ponto': 1},
+      ],
     }
   ];
 
-  void _responder() {
+  void _responder(int pontuacao) {
     if (temPerguntaSelecionada) {
       setState(() {
-        _perguntaselecionada++;
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
       });
     }
   }
 
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
+  }
+
   bool get temPerguntaSelecionada {
-    return _perguntaselecionada < _perguntas.length;
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
-        ? _perguntas[_perguntaselecionada]['respostas']
-        : [];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
         body: temPerguntaSelecionada
-            ? Column(children: [
-                Questao(_perguntas[_perguntaselecionada]['texto']),
-                ...respostas.map((t) => Resposta(t, _responder)).toList(),
-              ])
-            : Center(child: Text('Parabens!'),),
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                quandoResponder: _responder)
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
---
+++ lib/resultado.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+  const Resultado(this.pontuacao, this.quandoReiniciarQuestionario,
+      {super.key});
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabéns!';
+    } else if (pontuacao < 12) {
+      return 'Você é bom!';
+    } else if (pontuacao < 12) {
+      return 'Impressionante!';
+    } else {
+      return 'Nível Jedi';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.blue,
+          ),
+          onPressed: quandoReiniciarQuestionario,
+          child: const Text(
+            'Reiniciar?',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ],
+    );
+  }
+}
